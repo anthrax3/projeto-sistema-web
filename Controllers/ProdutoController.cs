088808d78efc9de248642dcb802a83d623545cf5
Ramon Barros <ramon@sagasistemas.com.br>
2017-08-23 22:42:43 -0300

    Adicionado package RandomTestValues e criado lista de produtos.

--- Controllers/ProdutoController.cs
+++ Controllers/ProdutoController.cs
@@ -3,11 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Ftec.Cadastro.Site.Factory;
+using Ftec.Cadastro.Site.Models;
 
 namespace Ftec.Cadastro.Site.Controllers
 {
-    public class ProdutoController
+    public class ProdutoController : Controller
     {
-        
+        public IActionResult Index() 
+        {
+            
+            var produtos = Factory.MoqFactory.GerarListaProdutos(10);
+            
+            // win
+            ViewBag.produtos = produtos;
+            //ViewData["produtos"] = produtos;
+
+            return View();
+        }
+
+        public IActionResult Novo() 
+        {
+            return View();
+        }
+
+        public IActionResult Gravar(string descricao, int quantidade) 
+        {
+            // Executa uma ação
+            return RedirectToAction("Index");
+        }
     }
 }
---
+++ Factory/MoqFactory.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RandomTestValues;
+using Ftec.Cadastro.Site.Models;
+
+namespace Ftec.Cadastro.Site.Factory
+{
+    public class MoqFactory
+    {
+        public static List<Models.Produto> GerarListaProdutos(int quantidadeProdutos) 
+        {
+            List<Produto> produtos;
+
+            produtos = RandomValue.List<Produto>(quantidadeProdutos);
+
+            return produtos;
+        }
+    }
+}
